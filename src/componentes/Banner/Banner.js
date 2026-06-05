aa620a7383cacaf5aa6e5c7cc364508a1c6058cb
cmurilo1 <contatocodegeniuss@gmail.com>
2026-06-04 22:41:02 -0300

    Estilizacao: Banner centrlizado e ajustes com css

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,13 @@
+import './Banner.css';
+
+//JSX:
+
+function Banner() {
+    return (
+    <header className =  "banner"> 
+    <img src="/imagens/imagens/banner.png" alt="Banner principal da pagina do organo" />
+    </header>
+)
+    
+}
+export default Banner;
