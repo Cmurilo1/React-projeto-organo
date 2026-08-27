b88a36aa261baf92b8863f00578be4a1a9376cca
cmurilo1 <contatocodegeniuss@gmail.com>
2026-08-26 23:29:13 -0300

    Corrige:uso do formulario

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
   return (
     <div className="App">
       <Banner />
+      <CampoTexto label="Formulario" placeholder="Digite seu nome" />
       <CampoTexto label="Nome" placeholder="Digite seu nome" />
       <CampoTexto label="Cargo" placeholder="Digite seu cargo" />
       <CampoTexto label="Imagem" placeholder="Digite a endereco da imagem" />
