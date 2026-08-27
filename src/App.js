eb8d98732ba776210aa1621bc58c3b7d8b160574
cmurilo1 <contatocodegeniuss@gmail.com>
2026-08-26 23:35:53 -0300

    Corrige: uso Formulario

--- src/App.js
+++ src/App.js
@@ -7,10 +7,10 @@ function App() {
   return (
     <div className="App">
       <Banner />
-      <CampoTexto label="Formulario" placeholder="Digite seu nome" />
+      <Formulario />  {/* ✅ Usa o componente Formulario de verdade! */}
       <CampoTexto label="Nome" placeholder="Digite seu nome" />
       <CampoTexto label="Cargo" placeholder="Digite seu cargo" />
-      <CampoTexto label="Imagem" placeholder="Digite a endereco da imagem" />
+      <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem" />
     </div>
   );
 }
