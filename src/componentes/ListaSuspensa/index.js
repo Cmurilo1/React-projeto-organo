8c855ce4f915cb18433299a8d76fefcbc17b812c
cmurilo1 <contatocodegeniuss@gmail.com>
2026-08-30 00:09:11 -0300

    Formulario completo:Botao,ListaSuspensa e ajustes finais

--- src/componentes/ListaSuspensa/index.js
+++ src/componentes/ListaSuspensa/index.js
@@ -1,16 +1,14 @@
-import './ListaSuspensa.css';
+import './ListaSuspensa.css'
 
 const ListaSuspensa = (props) => {
-    console.log(props.itens);
-    return (
-        <div className='lista-suspensa'>
-            <label>{props.label}</label>
-            <select>
-                {props.itens.map((item) =>  <option key={item}>{item}</option>)}
-            </select>
-        </div>
-    
-    
-    )
+  return (
+    <div className="lista-suspensa">
+      <label>{props.label}</label>
+      <select>
+        {props.itens.map(item => <option key={item}>{item}</option>)}
+      </select>
+    </div>
+  )
 }
-export default ListaSuspensa;
+
+export default ListaSuspensa
